809b8fcce01d3ecc9a439f37f4d323101e0e8ffc
sharmashivnath <sharmashivnath012@gmail.com>
2025-04-07 22:28:44 +0530

    Assignment_1.py

--- Assignment.py
+++ Assignment.py
@@ -1,5 +1,5 @@
 # Problem_1: Write a Python Program that does the following
-'''
+
 num1 =int(input('Enter the first number: '))
 num2 =int(input('Enter the second number: '))
 
@@ -7,7 +7,6 @@ print('Addition: ',(num1+num2))
 print('Subtraction: ',(num1-num2))
 print('Multiplication: ',(num1*num2))
 print('Division: ',(num1/num2))
-'''
 
 
 # Problem_2:
