bc5df1b052c639f61d8bb9ff1a8758302449d481
sharmashivnath <sharmashivnath012@gmail.com>
2025-04-07 22:33:16 +0530

    Task_1.py

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+# Task_1: Creating a programme that will do the following:
+# Addition 
+# Subtraction
+# Multiplication
+# Division
+
+num1 =int(input('Enter the first number: '))
+num2 =int(input('Enter the second number: '))
+
+print('Addition: ',(num1+num2))
+print('Subtraction: ',(num1-num2))
+print('Multiplication: ',(num1*num2))
+print('Division: ',(num1/num2))
